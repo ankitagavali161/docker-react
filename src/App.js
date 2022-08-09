56688926d874a481219fae3a0ea47562f38614ac
System Administrator <root@147dda9f5b0b.ant.amazon.com>
2022-08-09 16:28:07 +0530

    second

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>oh hi there  there!</p>
+        <p>Ank there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
