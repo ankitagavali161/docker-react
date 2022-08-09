5cd9ecd3f665dd03fc347eeb1cf6510238be845c
System Administrator <root@147dda9f5b0b.ant.amazon.com>
2022-08-09 20:40:25 +0530

    added Port expose for Elastic beanstalks

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Ank there!</p>
+        <p>Oh Hey there! Pickuplimes</p>
         <a
           className="App-link"
           href="https://reactjs.org"
